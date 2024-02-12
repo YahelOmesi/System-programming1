2e4e942418c8af91ead40395e00badde51bca2bf
YahelOmesi <155374161+YahelOmesi@users.noreply.github.com>
2024-02-12 15:26:06 +0200

    Update my_mat.h

--- my_mat.h
+++ my_mat.h
@@ -5,8 +5,6 @@
 
 void initialGraph(int graph[N][N]);
 int isTherePath(int graph[N][N],int i,int j);
-//void printGraph(int graph[][N]);
-void floydWarshell(int matrix[N][N]);
 int shortestPath(int graph[N][N],int i,int j);
 
 #endif
