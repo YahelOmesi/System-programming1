3a9dbf3c968a2027976bb73123ae408e19159290
YahelOmesi <155374161+YahelOmesi@users.noreply.github.com>
2024-02-12 15:25:38 +0200

    Update my_graph.c

--- my_graph.c
+++ my_graph.c
@@ -7,47 +7,33 @@ int main(){
     char choise;
     int i, j;
 
-    do{
-        printf("choose one option:\n");
-        printf("A - Enter matrix values:\n");
-        printf("B - Check if there is a path:\n");
-        printf("C - Find the shortest path:\n");
-        printf("D - Exit:\n");
-
         scanf(" %c", &choise);
+        
+        while (choise != 'D' && choise != EOF){
 
         switch (choise)
         {
         case 'A':
             initialGraph(graph);
-            //printGraph(graph);
             break;
 
         case 'B':
-            //printf(("Enter i and j\n"));
             scanf("%d %d", &i, &j);
-            isTherePath(graph,i,j);
+            int result = isTherePath(graph,i,j);
+            if(result == 1){
+                printf("true\n");
+            }else{
+            printf("false\n");
+            }
             break;
 
         case 'C':
-            //printf(("Enter i and j\n"));
             scanf("%d %d", &i, &j);
-            floydWarshell(graph);
-            printf("%d ",shortestPath(graph,i,j));
-            printf("\n");
+            printf("%d \n",shortestPath(graph,i,j));
             break;
 
-         case 'D':
-         case EOF:
-           printf(("Exit the program\n"));
-            break;
-
-        default:
-            printf("This choice is not available\n");
         }
-        while (getchar() != '\n');
+        scanf(" %c", &choise);
     } 
-    
-    while(choise != 'D');
     return 0;
 }
