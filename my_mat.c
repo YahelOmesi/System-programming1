15ed45110003fe601b5117e90cc4e275b51e15f0
YahelOmesi <155374161+YahelOmesi@users.noreply.github.com>
2024-02-13 15:25:51 +0200

    Update my_mat.c

--- my_mat.c
+++ my_mat.c
@@ -1,6 +1,7 @@
 #include "stdio.h"
 #include "my_mat.h"
 #include <limits.h>
+
 #define INF INT_MAX
 
 void initialGraph(int graph[N][N]){
@@ -11,33 +12,13 @@ void initialGraph(int graph[N][N]){
         }  
     }}
 
-    // void printGraph(int graph[N][N]){
-    // printf("Adjacency Matrix:\n");
-    // for(int i = 0; i < N ; i++){
-    //     for (int j = 0; j < N; j++)
-    //     {
-    //        printf("%d ", graph[i][j]);
-    //     } 
-    //     printf("\n"); 
-    // }}
 
-    int isTherePath(int graph[N][N],int i,int j){
-        return (graph[i][j] != 0);
-        
-    }
-
-
-    int shortestPath(int graph[N][N], int x, int y){
+    int isTherePath(int graph[N][N], int x, int y){
         int path[N][N];
-
         for(int i = 0; i < N; i++){
             for(int j = 0; j < N; j++){
 
-                 if (graph[i][j] == 0){
-                path[i][j] = INF;
-                }
-
-                if(i == j){
+                if(i == j){ // Main diagonal
                 path[i][j] = 0;
                 }
 
@@ -51,21 +32,56 @@ void initialGraph(int graph[N][N]){
         for(int k = 0; k < N; k++){
             for(int i = 0; i < N; i++){
                 for(int j = 0; j < N; j++){
-                    if(path[i][k] != INF && path[k][j] != INF && path[i][j] > path[i][k] + path[k][j]){
+
+                    if(i==j || path[i][k] == 0 || path[k][j] == 0)
+                        continue;
+                    
+
+                    if(path[i][j]==0 || path[i][k]+path[k][j]< path[i][j])
                         path[i][j] = path[i][k] + path[k][j];
-                    }
                 }
             }
         }
 
         if(path[x][y] == 0 || path[x][y] == INF){
-            return -1; // No path exists
+            return 0; // No path exists
+        } else{
+            return 1;}
+    }
+
+    int shortestPath(int graph[N][N], int x, int y){
+        int path[N][N];
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+
+
+                if(i == j){ // Main diagonal
+                path[i][j] = 0;
+                }
+
+                else{
+                    path[i][j] = graph[i][j];
+                }
+            }
+        }
+
+
+         for(int k = 0; k < N; k++){
+            for(int i = 0; i < N; i++){
+                for(int j = 0; j < N; j++){
+
+                    if(i==j || path[i][k] == 0 || path[k][j] == 0)
+                        continue;
+                    
+
+                    if(path[i][j]==0 || path[i][k]+path[k][j]< path[i][j])
+                        path[i][j] = path[i][k] + path[k][j];
+                }
+            }
+        }
+
+        if(path[x][y] == 0 || path[x][y] == INF){
+            return -1; 
         } else{
             return path[x][y];}
     }
-
-   
-        
-            
-        
-    
